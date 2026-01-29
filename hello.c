27cc30f7a8ed0596f03c8c417b0e377cc0e5bf61
Alyonka <alyonachurilina@gmail.com>
2026-01-29 23:43:51 +0300

    cycle in file Hello

--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello world!");
+    int n;
+    scanf ("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Hello world!\n");
+    }
     return 0;
 }
